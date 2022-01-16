3d8d9a2d83593c753f18e8bceaeafa5d38b0addc
saloumeh-67 <s.saaraabi@gmail.com>
2022-01-16 12:15:42 +0100

    first part of portfolio

---
+++ src/components/About.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hi, I'm Saloumeh.
+            <br className="hidden lg:inline-block" />
+            Coding is my new Hubby.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Throughout the last couple of years and as a middle-easterner woman,
+            I have paved my way into the Danish job market with restless effort.
+            Following my passion, I've been studying Frontend Web Dev in Hack Your Future
+            since May 2021.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+            >
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./profilePic.png"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Data.js
@@ -0,0 +1,76 @@
+export const projects = [
+  {
+    title: "Weather App",
+    subtitle: "Using HTML, CSS, JS",
+    description: "In this App you can check the weather base on the location",
+    image: "",
+    link: "https://github.com/saloumeh-67/hyf-homework/blob/master/javascript/javascript3/week1/weather.js",
+  },
+
+  {
+    title: "GitHub User Search",
+    subtitle: "Using HTML, CSS, JS",
+    description: "Find Github Users",
+    image: "",
+    link: "https://saloumeh-67.github.io/h",
+  },
+  {
+    title: "Who's the fastest",
+    subtitle: "Using HTML, CSS, JS",
+    description: "Who is the is the fastest on clicking.",
+    image: "",
+    link: "https://github.com/saloumeh-67/hyf-homework/blob/master/javascript/javascript2/week3/js2week3.html",
+  },
+  {
+    title: "meal-sharing",
+    subtitle: "Using ReactJs",
+    description: "Learn how to cook Different dishes from around the world",
+    image: "",
+    link: "https://github.com/saloumeh-67/meal-sharing",
+  },
+];
+export const skills = [
+  "JavaScript",
+  "ReactJS",
+  "HTML",
+  "CSS",
+  "NodeJS",
+  "ExpressJS",
+  "MongoDB",
+  "KNEX",
+  "MySQL",
+  "Git",
+  "RESTApi",
+  "Docker",
+];
+export const pictures =[
+    {picture1:"png"},
+];
+export const educations =[
+    { title:"HackYourFuture, Web Development ",
+      place:"Copenhagen, Denmark",
+      period:"May 2021 _ March 2022"
+},
+   {  title:"Graduated with Bachelor's Degree",
+      place:"Azad University, B.A. in Judicial Law Studies",
+      period:"2005 _ 2010"
+},
+];
+export const experience=[
+   
+    {
+        period: "July 2020 - present",
+        position: "Sale Assistance",
+        company: "ZARA, Field's, Copenhagen",
+        details: "Customer Service"
+    },
+    {
+        period: "September 2019 _ Jun 2020",
+        position: "Sale Assistance",
+        company: "Plantorama",
+        details: "Consultancy in the choice of plants and plant design for indoor and outdoor plants",
+    }
+];
+// export const testimonials =[
+
+// ];
