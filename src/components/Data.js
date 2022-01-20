a06dcad6aafb00f33bc3144bc90fed3bab3e353a
saloumeh-67 <s.saaraabi@gmail.com>
2022-01-20 18:24:51 +0100

    deploy

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,9 +1,13 @@
+import mealsharing from "../images/mealsharing.png";
+ import weatherApp from "../images/weatherApp.png";
+ import whoIsTheFastest from "../images/whoIsTheFastest.png";
+// import mealsharing from "../images/.png";
 export const projects = [
   {
     title: "Weather App",
     subtitle: "Using HTML, CSS, JS",
     description: "In this App you can check the weather base on the location",
-    image: "",
+    image: weatherApp,
     link: "https://github.com/saloumeh-67/hyf-homework/blob/master/javascript/javascript3/week1/weather.js",
   },
 
@@ -12,14 +16,14 @@ export const projects = [
     subtitle: "Using ReactJs",
     description:
       "Website to Learn how to cook Different dishes from around the world",
-    image: "",
-    link: "https://github.com/saloumeh-67/meal-sharing",
+    image: mealsharing,
+    link: "https://hyf-meal-sharing-apps.herokuapp.com/",
   },
   {
     title: "Game, Who's the fastest",
     subtitle: "Using HTML, CSS, JS",
     description: "Who is the is the fastest on clicking.",
-    image: "",
+    image: whoIsTheFastest,
     link: "https://github.com/saloumeh-67/hyf-homework/blob/master/javascript/javascript2/week3/Game.js",
   },
   {
@@ -44,31 +48,31 @@ export const skills = [
   "RESTApi",
   "Docker",
 ];
-export const pictures =[
-    {picture1:"png"},
+export const pictures = [{ picture1: "png" }];
+export const educations = [
+  {
+    title: "HackYourFuture, Web Development ",
+    place: "Copenhagen, Denmark",
+    period: "May 2021 _ March 2022",
+  },
+  {
+    title: "Graduated with Bachelor's Degree",
+    place: "Azad University, B.A. in Judicial Law Studies",
+    period: "2005 _ 2010",
+  },
 ];
-export const educations =[
-    { title:"HackYourFuture, Web Development ",
-      place:"Copenhagen, Denmark",
-      period:"May 2021 _ March 2022"
-},
-   {  title:"Graduated with Bachelor's Degree",
-      place:"Azad University, B.A. in Judicial Law Studies",
-      period:"2005 _ 2010"
-},
-];
-export const experience=[
-   
-    {
-        period: "July 2020 - present",
-        position: "Sale Assistance",
-        company: "ZARA, Field's, Copenhagen",
-        details: "Customer Service"
-    },
-    {
-        period: "September 2019 _ Jun 2020",
-        position: "Sale Assistance",
-        company: "Plantorama",
-        details: "Consultancy in the choice of plants and plant design for indoor and outdoor plants",
-    }
+export const experience = [
+  {
+    period: "July 2020 - present",
+    position: "Sale Assistance",
+    company: "ZARA, Field's, Copenhagen",
+    details: "Customer Service",
+  },
+  {
+    period: "September 2019 _ Jun 2020",
+    position: "Sale Assistance",
+    company: "Plantorama",
+    details:
+      "Consultancy in the choice of plants and plant design for indoor and outdoor plants",
+  },
 ];
